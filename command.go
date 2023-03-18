2a8901dfbe36944f22403dca555a871781bb26f6
haijima <k.haijima@gmail.com>
2023-03-18 23:36:26 +0900

    refactor: Refactor sort logic of commands

--- command.go
+++ command.go
@@ -258,18 +258,13 @@ func (c *Command) useEnv() {
 	})
 }
 
-// Sorts commands by their names.
-type commandSorterByName []*Command
-
-func (c commandSorterByName) Len() int           { return len(c) }
-func (c commandSorterByName) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c commandSorterByName) Less(i, j int) bool { return c[i].Name() < c[j].Name() }
-
 // Commands returns a sorted slice of child commands.
 func (c *Command) Commands() []*Command {
 	// do not sort commands if it already sorted or sorting was disabled
 	if cobra.EnableCommandSorting && !c.commandsAreSorted {
-		sort.Sort(commandSorterByName(c.commands))
+		sort.Slice(c.commands, func(i, j int) bool {
+			return c.commands[i].Name() < c.commands[j].Name()
+		})
 		c.commandsAreSorted = true
 	}
 	return c.commands
