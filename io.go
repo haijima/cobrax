5a9ea903712616285384424d551f8c1be33f5bec
haijima <k.haijima@gmail.com>
2024-03-21 22:07:08 +0900

    feat: Update OpenOrStdIn to consider whether it is terminal input

--- io.go
+++ io.go
@@ -1,19 +1,41 @@
 package cobrax
 
 import (
+	"errors"
 	"io"
+	"os"
 
 	"github.com/spf13/afero"
+	"golang.org/x/term"
 )
 
-func OpenOrStdIn(filename string, fs afero.Fs, stdin io.Reader) (io.ReadCloser, error) {
+var ErrNoFileSpecified = errors.New("no file specified")
+
+type Option struct {
+	stdin             io.Reader
+	enableManualInput bool
+}
+
+type OptionFunc func(*Option)
+
+func WithStdin(stdin io.Reader) OptionFunc { return func(o *Option) { o.stdin = stdin } }
+
+func WithManualInputEnabled(o *Option) { o.enableManualInput = true }
+
+func OpenOrStdIn(filename string, fs afero.Fs, opts ...OptionFunc) (io.ReadCloser, error) {
+	o := &Option{stdin: os.Stdin}
+	for _, f := range opts {
+		f(o)
+	}
 	if filename != "" {
 		f, err := fs.Open(filename)
 		if err != nil {
 			return nil, err
 		}
 		return f, nil
+	} else if f, ok := o.stdin.(*os.File); ok && !o.enableManualInput && term.IsTerminal(int(f.Fd())) {
+		return nil, ErrNoFileSpecified
 	} else {
-		return io.NopCloser(stdin), nil
+		return io.NopCloser(o.stdin), nil
 	}
 }
