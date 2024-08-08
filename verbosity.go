5911dbe468f2ea52dd7b535325433c7a7421f222
haijima <k.haijima@gmail.com>
2024-08-08 23:55:19 +0900

    feat: Add fop options to verbosity flag

--- verbosity.go
+++ verbosity.go
@@ -13,6 +13,29 @@ func VerbosityCount(v *viper.Viper) int {
 	return v.GetInt("verbose")
 }
 
-func VerbosityLevel(v *viper.Viper) slog.Level {
-	return slog.LevelError - slog.Level(4*VerbosityCount(v))
+func VerbosityLevel(v *viper.Viper, opts ...VerbosityLevelOption) slog.Level {
+	options := &VerbosityLevelOptions{zeroLevel: slog.LevelError, step: 4}
+	for _, o := range opts {
+		o(options)
+	}
+	return options.zeroLevel - slog.Level(options.step*VerbosityCount(v))
+}
+
+type VerbosityLevelOptions struct {
+	zeroLevel slog.Level
+	step      int
+}
+
+type VerbosityLevelOption func(*VerbosityLevelOptions)
+
+func WithVerbosityZeroLevel(level slog.Level) VerbosityLevelOption {
+	return func(o *VerbosityLevelOptions) {
+		o.zeroLevel = level
+	}
+}
+
+func WithVerbosityStep(step int) VerbosityLevelOption {
+	return func(o *VerbosityLevelOptions) {
+		o.step = step
+	}
 }
