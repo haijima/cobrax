1b894c588e683d6c19235da968d49fb14eeb6bf8
haijima <k.haijima@gmail.com>
2024-11-28 22:31:34 +0900

    feat: Add KebabToSnake

--- flag.go
+++ flag.go
@@ -10,3 +10,8 @@ import (
 func SnakeToKebab(f *pflag.FlagSet, name string) pflag.NormalizedName {
 	return pflag.NormalizedName(strings.Replace(name, "_", "-", -1))
 }
+
+// KebabToSnake normalizes flag names from kebab-case to snake_case.
+func KebabToSnake(f *pflag.FlagSet, name string) pflag.NormalizedName {
+	return pflag.NormalizedName(strings.Replace(name, "-", "_", -1))
+}
